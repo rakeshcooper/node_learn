989c9912d360dfcee2276f4d56e9099ad84d8d84
rakeshcooper <rakeshmurali097@gmail.com>
2024-11-26 01:49:52 +0530

    u

---
+++ server.mjs
@@ -0,0 +1,26 @@
+// console.log("hello cooper");
+
+// const os = require("os");
+// const path = require("path");
+
+// console.log(os.type());
+// console.log(os.version());
+// console.log(os.homedir());
+
+// console.log(__dirname);
+// console.log(__filename);
+// console.log(path.dirname(__filename));
+// console.log(path.parse(__filename));
+
+// const {add, sub, mul, div} = require("./math");
+// console.log(add(6, 3));
+// console.log(sub(6, 3));
+// console.log(mul(6, 3));
+// console.log(div(6, 3));
+
+import { readFile } from "node:fs";
+
+readFile("./files/file.txt", (err, data) => {
+  if (err) throw err;
+  console.log(data.toString());
+});
