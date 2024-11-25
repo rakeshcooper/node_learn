f9fbbb9defec70455a40c09b7777683414a9e719
rakeshcooper <rakeshmurali097@gmail.com>
2024-11-26 00:56:02 +0530

    update

--- server.js
+++ server.js
@@ -12,3 +12,9 @@ console.log(__dirname);
 console.log(__filename);
 console.log(path.dirname(__filename));
 console.log(path.parse(__filename));
+
+const math = require("./math");
+console.log(math.add(6, 3));
+console.log(math.sub(6, 3));
+console.log(math.mul(6, 3));
+console.log(math.div(6, 3));
