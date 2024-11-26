21075c9d94115822f9011082bf94c80e3d4843c8
rakeshcooper <rakeshmurali097@gmail.com>
2024-11-26 16:59:30 +0530

    update

--- server.js
+++ server.js
@@ -28,6 +28,32 @@ fs.readFile(path.join(__dirname, "files", "file.txt"), "utf8", (err, data) => {
   console.log(data);
 });
 
+fs.writeFile(
+  path.join(__dirname, "files", "new.txt"),
+  "cooper is good boy",
+  (err) => {
+    if (err) throw err;
+    console.log("write complete");
+    fs.appendFile(
+      path.join(__dirname, "files", "new.txt"),
+      "\n\n cooper is ate is lunch, this appended line",
+      (err) => {
+        if (err) throw err;
+        console.log("append complete");
+        fs.readFile(
+          path.join(__dirname, "files", "new.txt"),
+          "utf8",
+          (err, data) => {
+            if (err) throw err;
+            console.log(data);
+            console.log("new filed readed");
+          }
+        );
+      }
+    );
+  }
+);
+
 process.on("uncaughtException", (err) => {
   console.error(`Uncaught Exception thrown ${err}`);
   process.exit(1);
