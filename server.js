2897767bde29813da320b0926b42c893be5535cc
rakeshcooper <rakeshmurali097@gmail.com>
2024-11-26 17:33:21 +0530

    update

--- server.js
+++ server.js
@@ -3,56 +3,64 @@
 // const os = require("os");
 const path = require("path");
 
-// console.log(os.type());
-// console.log(os.version());
-// console.log(os.homedir());
+// const fs = require("node:fs");
+const fsPromise = require("fs").promises;
 
-// console.log(__dirname);
-// console.log(__filename);
-// console.log(path.dirname(__filename));
-// console.log(path.parse(__filename));
-
-// const {add, sub, mul, div} = require("./math");
-// console.log(add(6, 3));
-// console.log(sub(6, 3));
-// console.log(mul(6, 3));
-// console.log(div(6, 3));
-
-// import { readFile } from "node:fs";
-
-const { log } = require("node:console");
-const fs = require("node:fs");
-
-fs.readFile(path.join(__dirname, "files", "file.txt"), "utf8", (err, data) => {
-  if (err) throw err;
-  console.log(data);
-});
-
-fs.writeFile(
-  path.join(__dirname, "files", "new.txt"),
-  "cooper is good boy",
-  (err) => {
-    if (err) throw err;
-    console.log("write complete");
-    fs.appendFile(
-      path.join(__dirname, "files", "new.txt"),
-      "\n\n cooper is ate is lunch, this appended line",
-      (err) => {
-        if (err) throw err;
-        console.log("append complete");
-        fs.readFile(
-          path.join(__dirname, "files", "new.txt"),
-          "utf8",
-          (err, data) => {
-            if (err) throw err;
-            console.log(data);
-            console.log("new filed readed");
-          }
-        );
-      }
+async function fileops() {
+  try {
+    const data = await fsPromise.readFile(
+      path.join(__dirname, "files", "file.txt"),
+      "utf8"
     );
+    console.log(data);
+
+    await fsPromise.writeFile(
+      path.join(__dirname, "files", "new.txt"),
+      "cooper is good boy"
+    );
+    console.log("write complete");
+
+    await fsPromise.appendFile(
+      path.join(__dirname, "files", "new.txt"),
+      "\n\n cooper is ate is lunch, this appended line"
+    );
+    console.log("append complete");
+  } catch (err) {
+    console.error(err);
   }
-);
+}
+fileops();
+
+// fs.readFile(path.join(__dirname, "files", "file.txt"), "utf8", (err, data) => {
+//   if (err) throw err;
+//   console.log(data);
+// });
+
+// fs.writeFile(
+//   path.join(__dirname, "files", "new.txt"),
+//   "cooper is good boy",
+//   (err) => {
+//     if (err) throw err;
+//     console.log("write complete");
+//     fs.appendFile(
+//       path.join(__dirname, "files", "new.txt"),
+//       "\n\n cooper is ate is lunch, this appended line",
+//       (err) => {
+//         if (err) throw err;
+//         console.log("append complete");
+//         fs.rename(
+//           path.join(__dirname, "files", "new.txt"),
+//           path.join(__dirname, "files", "update.txt"),
+//           (err, data) => {
+//             if (err) throw err;
+//             console.log(data);
+//             console.log("rename done");
+//           }
+//         );
+//       }
+//     );
+//   }
+// );
 
 process.on("uncaughtException", (err) => {
   console.error(`Uncaught Exception thrown ${err}`);
