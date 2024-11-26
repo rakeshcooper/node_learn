af39cc5e1445b45a0aff03ef5517a212ef13e5ac
rakeshcooper <rakeshmurali097@gmail.com>
2024-11-26 19:43:03 +0530

    update

---
+++ stream.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const path = require("path");
+
+const rs = fs.createReadStream(path.join(__dirname, "files", "big_file.txt"), {
+  encoding: "utf-8",
+});
+
+const ws = fs.createWriteStream(
+  path.join(__dirname, "files", "new_big_file.txt")
+);
+
+rs.on("data", (datachunk) => {
+  ws.write(datachunk);
+});
