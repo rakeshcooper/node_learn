0dc71ee9c5db055be47c8397dba022bf7aa42549
rakeshcooper <rakeshmurali097@gmail.com>
2024-11-27 15:45:20 +0530

    update

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const logEvents = require("./logEvents");
+const EventEmitter = require("node:events");
+
+class MyEmitter extends EventEmitter {}
+
+const myEmitter = new MyEmitter();
+myEmitter.on("event", () => {
+  console.log("an event occurred!");
+});
+myEmitter.emit("event");
