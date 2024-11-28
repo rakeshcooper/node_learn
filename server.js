f84747c7aa73bf6edeb1d641683b4701b27f8793
rakeshcooper <rakeshmurali097@gmail.com>
2024-11-28 14:54:42 +0530

    update

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const http = require("http");
+const fs = require("fs");
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-  res.write("Hi cooper");
-  res.end();
+  res.writeHead(200, { "content-type": "text/html" });
+  fs.readFile("index.html", (error, data) => {
+    if (error) {
+      res.writeHead(404);
+      res.write("file not found");
+    } else {
+      res.write(data);
+    }
+    res.end();
+  });
+  //   res.write("Hi cooper");
 });
 
 server.listen(PORT, (error) => {
